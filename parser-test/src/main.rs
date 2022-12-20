18244a24ae173ac73e8943f14038a7524fabc9ec
Serial <69764315+Serial-ATA@users.noreply.github.com>
2022-12-20 15:11:06 -0500

    Delete `parser-test` crate

--- parser-test/src/main.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-	let path = std::env::args().nth(1).unwrap();
-	let mut f = std::fs::File::open(path).unwrap();
-
-	let class = class_parser::parse_class(&mut f);
-
-	println!("{:#?}", class);
-}
