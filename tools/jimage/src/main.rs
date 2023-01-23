09a47b5588ee8a79ebf609ebf2435f6e845d0899
Serial <69764315+Serial-ATA@users.noreply.github.com>
2023-01-23 10:39:14 -0500

    tools/jimage: Implement list subcommand

--- tools/jimage/src/main.rs
+++ tools/jimage/src/main.rs
@@ -1,4 +1,10 @@
+#![feature(let_chains)]
+
+use std::fs;
+use std::path::PathBuf;
+
 use clap::Parser;
+use jimage::JImage;
 
 #[derive(Parser)]
 #[command(
@@ -21,9 +27,7 @@ regex:<regex-pattern>"
 	include: Option<Vec<String>>,
 
 	#[command(subcommand)]
-	command: Option<SubCommand>,
-	#[arg(required = true, help = "The path to the jimage file")]
-	jimage: String,
+	command: SubCommand,
 }
 
 #[derive(Debug, clap::Subcommand)]
@@ -32,27 +36,102 @@ enum SubCommand {
 	Extract {
 		#[arg(long, default_value = ".")]
 		dir: String,
+		#[arg(required = true, help = "The path to the jimage file")]
+		jimage: PathBuf,
 	},
 	/// Prints detailed information contained in the jimage header.
-	Info,
+	Info {
+		#[arg(required = true, help = "The path to the jimage file")]
+		jimage: PathBuf,
+	},
 	/// Prints the names of all the entries in the jimage.
 	///
 	/// When used with --verbose, list will also print entry size and offset attributes.
 	List {
 		#[arg(long)]
 		verbose: bool,
+		#[arg(required = true, help = "The path to the jimage file")]
+		jimage: PathBuf,
 	},
 	/// Reports on any .class entries that dont verify as classes.
-	Verify,
+	Verify {
+		#[arg(required = true, help = "The path to the jimage file")]
+		jimage: PathBuf,
+	},
 }
 
 fn main() {
-	let _args = Command::parse();
+	let args = Command::parse();
 
+	match args.command {
+		SubCommand::List { verbose, jimage } => list(jimage, verbose),
+		c => unimplemented!("{:?}", c),
+	}
 	// TODO: glob includes
 	// TODO: regex includes
 	// TODO: extract subcommand
 	// TODO: info subcommand
-	// TODO: list subcommand
 	// TODO: verify subcommand
+}
+
+fn list(path: PathBuf, verbose: bool) {
+	let jimage = jimage_parser::parse(&mut fs::File::open(&path).unwrap());
+
+	println!("jimage: {:?}", fs::canonicalize(path).unwrap());
+
+	let mut old_module = String::new();
+	for name in jimage.get_entry_names().unwrap() {
+		if !JImage::is_tree_info_resource(&name) {
+			let new_module = module_name(&name);
+
+			if new_module != old_module {
+				list_module(new_module, verbose);
+				old_module = new_module.to_string();
+			}
+
+			print(&name, verbose.then_some(&jimage));
+		}
+	}
+}
+
+fn module_name(path: &str) -> &str {
+	let offset = path[1..].find('/');
+	match offset {
+		Some(offset) => &path[1..=offset],
+		None => "<unknown>",
+	}
+}
+
+fn trim_module(name: &str) -> &str {
+	let offset = name[1..].find('/').map(|offset| offset + 1);
+	if let Some(offset) = offset && offset + 1 < name.len() {
+		return &name[offset+1..];
+	}
+
+	name
+}
+
+fn print(name: &str, image: Option<&JImage>) {
+	if let Some(image) = image {
+		let Some(location) = image.find_location(name) else {
+			return
+		};
+
+		print!("{:>12} ", location.get_content_offset());
+		print!("{:>10} ", location.get_uncompressed_size());
+		print!("{:>10} ", location.get_compressed_size());
+	}
+
+	println!("{}", trim_module(name));
+}
+
+fn list_module(module: &str, verbose: bool) {
+	println!("\nModule: {}", module);
+
+	if verbose {
+		print!("Offset       ");
+		print!("Size       ");
+		print!("Compressed ");
+		println!("Entry")
+	}
 }
