363875deb8806c142adc2ff6e8d4df31474f25b9
Serial <69764315+Serial-ATA@users.noreply.github.com>
2023-01-28 13:59:53 -0500

    runtime/native: Implement java.lang.System#nanoTime

--- runtime/src/native/java/lang/System.rs
+++ runtime/src/native/java/lang/System.rs
@@ -1,6 +1,11 @@
 use crate::native::NativeReturn;
 use crate::stack::local_stack::LocalStack;
 
+use std::time::{SystemTime, UNIX_EPOCH};
+
+use common::int_types::s8;
+use instructions::Operand;
+
 include!("def/System.registerNatives");
 
 pub fn setIn0(_: LocalStack) -> NativeReturn {
@@ -17,7 +22,12 @@ pub fn currentTimeMillis(_: LocalStack) -> NativeReturn {
 	unimplemented!("System#currentTimeMillis")
 }
 pub fn nanoTime(_: LocalStack) -> NativeReturn {
-	unimplemented!("System#nanoTime")
+	let time_nanos = SystemTime::now()
+		.duration_since(UNIX_EPOCH)
+		.expect("current system time should not be before the UNIX epoch")
+		.as_nanos();
+
+	Some(Operand::Long(time_nanos as s8))
 }
 
 pub fn arraycopy(_: LocalStack) -> NativeReturn {
