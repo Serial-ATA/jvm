967507e818d2e524bdc941b1b79d6413cb5d382b
Serial <69764315+Serial-ATA@users.noreply.github.com>
2023-01-28 20:17:03 -0500

    runtime/native: Implement java.lang.Double natives

--- runtime/src/native/java/lang/Double.rs
+++ runtime/src/native/java/lang/Double.rs
@@ -1,9 +1,14 @@
 use crate::native::NativeReturn;
 use crate::stack::local_stack::LocalStack;
 
-pub fn doubleToRawLongBits(_: LocalStack) -> NativeReturn {
-	unimplemented!("java.lang.Double#doubleToRawLongBits")
+use common::int_types::{s8, u8};
+use instructions::Operand;
+
+pub fn doubleToRawLongBits(locals: LocalStack) -> NativeReturn {
+	let double = locals[0].expect_double();
+	Some(Operand::Long(double.to_bits() as s8))
 }
-pub fn longBitsToDouble(_: LocalStack) -> NativeReturn {
-	unimplemented!("java.lang.Double#longBitsToDouble")
+pub fn longBitsToDouble(locals: LocalStack) -> NativeReturn {
+	let long = locals[0].expect_long();
+	Some(Operand::Double(f64::from_bits(long as u8)))
 }
