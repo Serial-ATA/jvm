b4c509663536a331916faade6725b4eab266138b
Serial <69764315+Serial-ATA@users.noreply.github.com>
2023-01-28 19:50:54 -0500

    runtime/native: Implement java.lang.Float#floatToRawIntBits

--- runtime/src/native/java/lang/Float.rs
+++ runtime/src/native/java/lang/Float.rs
@@ -1,8 +1,12 @@
 use crate::native::NativeReturn;
 use crate::stack::local_stack::LocalStack;
 
-pub fn floatToRawIntBits(_: LocalStack) -> NativeReturn {
-	unimplemented!("java.lang.Float#floatToRawIntBits")
+use common::int_types::s4;
+use instructions::Operand;
+
+pub fn floatToRawIntBits(locals: LocalStack) -> NativeReturn {
+	let float = locals[0].expect_float();
+	Some(Operand::Int(float.to_bits() as s4))
 }
 pub fn intBitsToFloat(_: LocalStack) -> NativeReturn {
 	unimplemented!("java.lang.Float#intBitsToFloat")
