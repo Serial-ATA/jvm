eea8e5ba8d425023ecce79a3b85947128c76c0da
Serial <69764315+Serial-ATA@users.noreply.github.com>
2023-02-12 13:26:33 -0500

    runtime/native: Partially implement jdk.internal.reflect.Reflection#getCallerClass

--- runtime/src/native/jdk/internal/reflect/Reflection.rs
+++ runtime/src/native/jdk/internal/reflect/Reflection.rs
@@ -1,8 +1,40 @@
 use crate::native::{JNIEnv, NativeReturn};
+use crate::reference::Reference;
 use crate::stack::local_stack::LocalStack;
 
-pub fn getCallerClass(_: JNIEnv, _: LocalStack) -> NativeReturn {
-	unimplemented!("jdk.internal.reflect.Reflection#getCallerClass")
+use common::traits::PtrType;
+use instructions::Operand;
+
+#[expect(clippy::match_same_arms)]
+pub fn getCallerClass(env: JNIEnv, _: LocalStack) -> NativeReturn {
+	for (n, frame) in env
+		.current_thread
+		.get()
+		.frame_stack
+		.iter()
+		.rev()
+		.enumerate()
+	{
+		let method = frame.method();
+		match n {
+			// TODO:
+			//   https://github.com/openjdk/jdk/blob/6a44120a16d0f06b4ed9f0ebf6b0919da7070287/src/hotspot/share/prims/jvm.cpp#L742-L744
+			//   https://github.com/openjdk/jdk/blob/6a44120a16d0f06b4ed9f0ebf6b0919da7070287/src/java.base/share/classes/java/lang/invoke/MethodHandleNatives.java#L117
+			0 => {},
+			// TODO:
+			//   https://github.com/openjdk/jdk/blob/6a44120a16d0f06b4ed9f0ebf6b0919da7070287/src/hotspot/share/prims/jvm.cpp#L748-L750
+			//   https://github.com/openjdk/jdk/blob/6a44120a16d0f06b4ed9f0ebf6b0919da7070287/src/java.base/share/classes/java/lang/invoke/MethodHandleNatives.java#L117
+			1 => {},
+			_ => {
+				// TODO: https://github.com/openjdk/jdk/blob/6a44120a16d0f06b4ed9f0ebf6b0919da7070287/src/hotspot/share/oops/method.cpp#L1378
+				return Some(Operand::Reference(Reference::Mirror(
+					method.class.get_mirror(),
+				)));
+			},
+		}
+	}
+
+	Some(Operand::Reference(Reference::Null))
 }
 pub fn getClassAccessFlags(_: JNIEnv, _: LocalStack) -> NativeReturn {
 	unimplemented!("jdk.internal.reflect.Reflection#getClassAccessFlags")
