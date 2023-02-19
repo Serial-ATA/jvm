71e121cf3d8b846e70230dd3db3e9bc97ec4f700
Serial <69764315+Serial-ATA@users.noreply.github.com>
2023-02-19 14:58:21 -0500

    runtime/native: Partially implement java.lang.Runtime#{availableProcessors, maxMemory}

--- runtime/src/native/java/lang/Runtime.rs
+++ runtime/src/native/java/lang/Runtime.rs
@@ -1,8 +1,11 @@
 use crate::native::{JNIEnv, NativeReturn};
 use crate::stack::local_stack::LocalStack;
 
+use common::int_types::{s4, s8};
+use instructions::Operand;
+
 pub fn availableProcessors(_: JNIEnv, _: LocalStack) -> NativeReturn {
-	unimplemented!("java.lang.Runtime#availableProcessors")
+	Some(Operand::Int(num_cpus::get() as s4))
 }
 pub fn freeMemory(_: JNIEnv, _: LocalStack) -> NativeReturn {
 	unimplemented!("java.lang.Runtime#freeMemory")
@@ -11,7 +14,8 @@ pub fn totalMemory(_: JNIEnv, _: LocalStack) -> NativeReturn {
 	unimplemented!("java.lang.Runtime#totalMemory")
 }
 pub fn maxMemory(_: JNIEnv, _: LocalStack) -> NativeReturn {
-	unimplemented!("java.lang.Runtime#maxMemory")
+	// TODO: Xmx
+	Some(Operand::Long(s8::MAX))
 }
 pub fn gc(_: JNIEnv, _: LocalStack) -> NativeReturn {
 	unimplemented!("java.lang.Runtime#gc")
