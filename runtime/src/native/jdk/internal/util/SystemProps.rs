6b068f55d4fd2daba3f67c7b18d6e71f1ad86c44
Serial <69764315+Serial-ATA@users.noreply.github.com>
2023-05-04 01:42:23 -0400

    runtime/native: Add SystemProps$Raw constant fields

--- runtime/src/native/jdk/internal/util/SystemProps.rs
+++ runtime/src/native/jdk/internal/util/SystemProps.rs
@@ -1,4 +1,6 @@
 pub mod Raw {
+	include!("def/SystemProps$Raw.constants");
+
 	use crate::class_instance::ArrayInstance;
 	use crate::classpath::classloader::ClassLoader;
 	use crate::native::{JNIEnv, NativeReturn};
@@ -10,14 +12,14 @@ pub mod Raw {
 	// TODO
 	pub fn vmProperties(_: JNIEnv, _: LocalStack) -> NativeReturn {
 		let string_array_class = ClassLoader::Bootstrap.load(b"[Ljava/lang/Object;").unwrap();
-		let prop_array = ArrayInstance::new_reference(39, string_array_class);
+		let prop_array = ArrayInstance::new_reference(FIXED_LENGTH, string_array_class);
 		Some(Operand::Reference(Reference::Array(prop_array)))
 	}
 
 	// TODO: https://github.com/openjdk/jdk/blob/19373b2ff0cd795afa262c17dcb3388fd6a5be59/src/java.base/share/native/libjava/System.c#L107
 	pub fn platformProperties(_: JNIEnv, _: LocalStack) -> NativeReturn {
 		let string_array_class = ClassLoader::Bootstrap.load(b"[Ljava/lang/String;").unwrap();
-		let prop_array = ArrayInstance::new_reference(39, string_array_class);
+		let prop_array = ArrayInstance::new_reference(FIXED_LENGTH, string_array_class);
 		Some(Operand::Reference(Reference::Array(prop_array)))
 	}
 }
