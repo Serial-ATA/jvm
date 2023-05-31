aadd5270110786e2cd9be718b0210784a0d80c7b
Serial <69764315+Serial-ATA@users.noreply.github.com>
2023-05-31 09:40:32 -0400

    runtime/heap: Fully implement interface method selection (§5.4.6)

--- runtime/src/heap/class.rs
+++ runtime/src/heap/class.rs
@@ -783,13 +783,54 @@ impl Class {
 	/// Find an implementation of an interface method on the target class
 	pub fn map_interface_method(class: ClassRef, method: MethodRef) -> MethodRef {
 		if let ClassType::Instance(instance) = &class.get().class_ty {
+			// https://docs.oracle.com/javase/specs/jvms/se19/html/jvms-5.html#jvms-5.4.6
+
+			// During execution of an invokeinterface or invokevirtual instruction, a method is
+			// selected with respect to:
+			//
+			// (i) the run-time type of the object on the stack, and
+			// (ii) a method that was previously resolved by the instruction.
+			//
+			// The rules to select a method with respect to a class or interface C and a method mR are as follows:
+
+			// 1. If mR is marked ACC_PRIVATE, then it is the selected method.
+			if method.is_private() {
+				return method;
+			}
+
+			// 2. Otherwise, the selected method is determined by the following lookup procedure:
+
+			//    If C contains a declaration of an instance method m that can override mR (§5.4.5), then m is the selected method.
 			for instance_method in &instance.methods {
-				if instance_method.name == method.name
-					&& instance_method.descriptor == method.descriptor
-				{
+				if instance_method.can_override(&*method) {
 					// We found an implementation
 					return Arc::clone(instance_method);
 				}
+			}
+
+			//    Otherwise, if C has a superclass, a search for a declaration of an instance method that can override mR is performed,
+			//    starting with the direct superclass of C and continuing with the direct superclass of that class, and so forth, until a
+			//    method is found or no further superclasses exist. If a method is found, it is the selected method.
+			for parent in class.parent_iter() {
+				if let ClassType::Instance(instance) = &parent.get().class_ty {
+					for instance_method in &instance.methods {
+						if instance_method.can_override(&*method) {
+							// We found an implementation
+							return Arc::clone(instance_method);
+						}
+					}
+				}
+			}
+
+			//    Otherwise, the maximally-specific superinterface methods of C are determined (§5.4.3.3). If exactly one matches mR's name
+			//    and descriptor and is not abstract, then it is the selected method.
+			if let Some(superinterface_method) = Class::resolve_method_in_superinterfaces(
+				&method.name,
+				&method.descriptor,
+				class,
+				true,
+			) {
+				return superinterface_method;
 			}
 		}
 
