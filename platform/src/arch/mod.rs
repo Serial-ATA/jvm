c0fe844b5557339e4af66b1421c1bd3cbdbec3dd
Serial <69764315+Serial-ATA@users.noreply.github.com>
2023-07-16 16:53:47 -0400

    platform: Separate x86 and x86_64

--- platform/src/arch/mod.rs
+++ platform/src/arch/mod.rs
@@ -1,11 +1,17 @@
-use crate::macros::conditional;
+use crate::macros::match_cfg_meta;
 
-conditional! {
-	#[cfg(any(
-		target_arch = "x86",
-		target_arch = "x86_64"
-	))]
-
-	mod x86;
-	pub use x86::*;
+match_cfg_meta! {
+	match cfg(target_arch) {
+		"x86" => {
+			mod x86;
+			pub use x86::*;
+		},
+		"x86_64" => {
+			mod x86_64;
+			pub use x86_64::*;
+		},
+		_ => {
+			compile_error!("target architecture is not supported!");
+		}
+	}
 }
