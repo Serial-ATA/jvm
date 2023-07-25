5a12c9f46cca637d9f8e7c3349e50cbf5d539f78
Serial <69764315+Serial-ATA@users.noreply.github.com>
2023-07-25 10:20:11 -0400

    generators/asm_specs: Parse Intel XED map descriptions

--- generators/asm_specs/x86/global_defs.py
+++ generators/asm_specs/x86/global_defs.py
@@ -1,6 +1,8 @@
+from generators.asm_specs.x86.map_info import MapInfo
 from generators.asm_specs.x86.width import Width
 from generators.asm_specs.x86.xtype import XType
 
 widths: dict[str, Width] = {}
 states: dict[str, str] = {}
 xtypes: dict[str, XType] = {}
+map_info: [MapInfo] = []
--- generators/asm_specs/x86/x86.py
+++ generators/asm_specs/x86/x86.py
@@ -5,6 +5,7 @@ from generators.asm_specs.util import generated_directory_for
 
 from generators.asm_specs.x86 import global_defs
 from generators.asm_specs.x86.instruction import InstructionParser, Instruction
+from generators.asm_specs.x86.map_info import MapInfoParser
 from generators.asm_specs.x86.text_utils import remove_comment_from_line
 from generators.asm_specs.x86.width import Width
 from generators.asm_specs.x86.xtype import XType
@@ -74,6 +75,19 @@ def parse_xtypes_from(path: Path):
     print("INFO: Parsed " + str(len(global_defs.xtypes)) + " XType definitions")
 
 
+def parse_map_descriptions_from(path: Path):
+    print("INFO: Parsing Intel XED map descriptions from: " + str(path.resolve()))
+    with open(path, "r") as file:
+        lines: Iterable[str] = map(remove_comment_from_line, iter(file.readlines()))
+        parser = MapInfoParser(lines)
+        while True:
+            parsed_map_info = parser.parse()
+            if not parsed_map_info:
+                break
+            global_defs.map_info.append(parsed_map_info)
+    print("INFO: Parsed " + str(len(global_defs.map_info)) + " map descriptions")
+
+
 def main():
     all_widths_path: Path = DGEN_DIR.joinpath("all-widths.txt")
     parse_widths_from(all_widths_path)
@@ -83,6 +97,9 @@ def main():
 
     all_element_types_path: Path = DGEN_DIR.joinpath("all-element-types.txt")
     parse_xtypes_from(all_element_types_path)
+
+    all_map_descriptions: Path = DGEN_DIR.joinpath("all-map-descriptions.txt")
+    parse_map_descriptions_from(all_map_descriptions)
 
     all_dec_intructions_path: Path = DGEN_DIR.joinpath("all-dec-instructions.txt")
     instructions = parse_instructions_from(all_dec_intructions_path)
