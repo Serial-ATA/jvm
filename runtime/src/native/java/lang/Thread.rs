3bfa32faefed8990e3221a9643712908dc080f59
Serial <69764315+Serial-ATA@users.noreply.github.com>
2025-12-08 02:17:00 -0500

    runtime/native: transition some java.lang.Thread natives to JNI
    Only moved over a few methods needed for early boot.

--- runtime/src/native/java/lang/Thread.rs
+++ runtime/src/native/java/lang/Thread.rs
@@ -7,12 +7,10 @@ use crate::thread::pool::ThreadPool;
 use crate::thread::{JavaThread, JavaThreadBuilder};
 
 use std::cmp;
-use std::sync::atomic::AtomicUsize;
 
 use ::jni::env::JniEnv;
-use ::jni::sys::{jboolean, jint, jlong};
+use ::jni::sys::{jboolean, jlong};
 
-include_generated!("native/java/lang/def/Thread.registerNatives.rs");
 include_generated!("native/java/lang/def/Thread.definitions.rs");
 
 pub fn findScopedValueBindings(_env: JniEnv, _class: ClassPtr) -> Reference /* java.lang.Object */
@@ -23,14 +21,6 @@ pub fn findScopedValueBindings(_env: JniEnv, _class: ClassPtr) -> Reference /* j
 pub fn currentCarrierThread(_env: JniEnv, _class: ClassPtr) -> Reference /* java.lang.Thread */
 {
 	unimplemented!("java.lang.Thread#currentCarrierThread");
-}
-
-pub fn currentThread(env: JniEnv, _class: ClassPtr) -> Reference /* java.lang.Thread */
-{
-	unsafe {
-		let thread = JavaThread::for_env(env.raw().cast_const());
-		(*thread).obj().expect("current thread should exist")
-	}
 }
 
 pub fn setCurrentThread(
@@ -54,14 +44,6 @@ pub fn setScopedValueCache(
 	unimplemented!("java.lang.Thread#setScopedValueCache");
 }
 
-pub fn ensureMaterializedForStackWalk(
-	_env: JniEnv,
-	_class: ClassPtr,
-	_o: Reference, // java.lang.Object
-) {
-	// Nothing to do
-}
-
 pub fn yield0(_env: JniEnv, _class: ClassPtr) {
 	std::thread::yield_now();
 }
@@ -69,35 +51,6 @@ pub fn yield0(_env: JniEnv, _class: ClassPtr) {
 // throws InterruptedException
 pub fn sleepNanos0(_env: JniEnv, _class: ClassPtr, _nanos: jlong) {
 	unimplemented!("java.lang.Thread#sleepNanos0");
-}
-
-pub fn start0(_env: JniEnv, this: Reference /* java.lang.Thread */) {
-	{
-		if let Some(existing_thread) = ThreadPool::find_from_obj(this) {
-			throw!(existing_thread, IllegalThreadStateException);
-		}
-	}
-
-	let holder = classes::java::lang::Thread::holder(this.extract_class());
-	let stack_size_raw = classes::java::lang::Thread::holder::stackSize(holder.extract_class());
-
-	let mut thread_builder = JavaThreadBuilder::new()
-		.obj(this)
-		.entry_point(JavaThread::default_entry_point);
-
-	if stack_size_raw > 0 {
-		let stack_size = cmp::min(stack_size_raw as usize, u32::MAX as usize);
-		thread_builder = thread_builder.stack_size(stack_size);
-	}
-
-	let thread = thread_builder.finish();
-
-	let obj = thread.obj().expect("current thread object should exist");
-	let holder = classes::java::lang::Thread::holder(obj.extract_class());
-	classes::java::lang::Thread::holder::set_threadStatus(
-		holder.extract_class(),
-		ThreadStatus::Runnable,
-	);
 }
 
 pub fn holdsLock(
@@ -130,23 +83,6 @@ pub fn getThreads(_env: JniEnv, _class: ClassPtr) -> Reference /* []java.lang.Th
 	unimplemented!("java.lang.Thread#getThreads");
 }
 
-pub fn setPriority0(
-	_env: JniEnv,
-	this: Reference, // java.lang.Thread
-	new_priority: jint,
-) {
-	let holder = classes::java::lang::Thread::holder(this.extract_class());
-	classes::java::lang::Thread::holder::set_priority(holder.extract_class(), new_priority);
-
-	let java_thread = ThreadPool::find_from_obj(this);
-	let Some(_thread) = java_thread else {
-		return;
-	};
-
-	// Thread is alive...
-	todo!("Set priority on JavaThread?")
-}
-
 pub fn interrupt0(_env: JniEnv, _this: Reference /* java.lang.Thread */) {
 	unimplemented!("java.lang.Thread#interrupt");
 }
@@ -161,12 +97,4 @@ pub fn setNativeName(
 	_name: Reference, // java.lang.String
 ) {
 	unimplemented!("java.lang.Thread#setNativeName");
-}
-
-pub fn getNextThreadIdOffset(_env: JniEnv, _class: ClassPtr) -> jlong {
-	// https://github.com/openjdk/jdk/blob/a3b58ee5cd1ec0ea78649d4128d272458b05eb13/src/java.base/share/classes/java/lang/Thread.java#L624-L627
-	const INITIAL_THREAD_ID: usize = 3;
-	static NEXT_THREAD_ID: AtomicUsize = AtomicUsize::new(INITIAL_THREAD_ID);
-
-	NEXT_THREAD_ID.as_ptr() as jlong
 }
--- runtime/src/native/jvm/threads.rs
+++ runtime/src/native/jvm/threads.rs
@@ -1,18 +1,70 @@
 #![native_macros::jni_fn_module]
 
-use jni::env::JniEnv;
-use jni::objects::{JClass, JObject, JObjectArray, JString};
-use jni::sys::{jboolean, jint, jlong};
+use crate::classes;
+use crate::classes::java::lang::Thread::ThreadStatus;
+use crate::native::jni::{IntoJni, reference_from_jobject};
+use crate::thread::exceptions::throw;
+use crate::thread::pool::ThreadPool;
+use crate::thread::{JavaThread, JavaThreadBuilder};
+
+use std::cmp;
+use std::sync::atomic::AtomicUsize;
+
+use ::jni::env::JniEnv;
+use ::jni::objects::{JClass, JObject, JObjectArray, JString};
+use ::jni::sys::{jboolean, jint, jlong};
 use native_macros::jni_call;
 
 #[jni_call]
-pub extern "C" fn JVM_StartThread(_env: JniEnv, _thread: JObject) {
-	todo!()
+pub extern "C" fn JVM_StartThread(_env: JniEnv, this: JObject) {
+	let Some(this) = (unsafe { reference_from_jobject(this.raw()) }) else {
+		return; // TODO: Exception?
+	};
+
+	{
+		if let Some(existing_thread) = ThreadPool::find_from_obj(this) {
+			throw!(existing_thread, IllegalThreadStateException);
+		}
+	}
+
+	let holder = classes::java::lang::Thread::holder(this.extract_class());
+	let stack_size_raw = classes::java::lang::Thread::holder::stackSize(holder.extract_class());
+
+	let mut thread_builder = JavaThreadBuilder::new()
+		.obj(this)
+		.entry_point(JavaThread::default_entry_point);
+
+	if stack_size_raw > 0 {
+		let stack_size = cmp::min(stack_size_raw as usize, u32::MAX as usize);
+		thread_builder = thread_builder.stack_size(stack_size);
+	}
+
+	let thread = thread_builder.finish();
+
+	let obj = thread.obj().expect("current thread object should exist");
+	let holder = classes::java::lang::Thread::holder(obj.extract_class());
+	classes::java::lang::Thread::holder::set_threadStatus(
+		holder.extract_class(),
+		ThreadStatus::Runnable,
+	);
 }
 
 #[jni_call]
-pub extern "C" fn JVM_SetThreadPriority(_env: JniEnv, _thread: JObject, _priority: jint) {
-	todo!()
+pub extern "C" fn JVM_SetThreadPriority(_env: JniEnv, this: JObject, priority: jint) {
+	let Some(this) = (unsafe { reference_from_jobject(this.raw()) }) else {
+		return; // TODO: Exception?
+	};
+
+	let holder = classes::java::lang::Thread::holder(this.extract_class());
+	classes::java::lang::Thread::holder::set_priority(holder.extract_class(), priority);
+
+	let java_thread = ThreadPool::find_from_obj(this);
+	let Some(_thread) = java_thread else {
+		return;
+	};
+
+	// Thread is alive...
+	todo!("Set priority on JavaThread?")
 }
 
 #[jni_call]
@@ -31,8 +83,12 @@ pub extern "C" fn JVM_CurrentCarrierThread(_env: JniEnv, _class: JClass) -> JObj
 }
 
 #[jni_call]
-pub extern "C" fn JVM_CurrentThread(_env: JniEnv, _class: JClass) -> JObject {
-	todo!()
+pub extern "C" fn JVM_CurrentThread(env: JniEnv, _class: JClass) -> JObject {
+	let thread = unsafe { &*JavaThread::for_env(env.raw().cast_const()) };
+	thread
+		.obj()
+		.expect("current thread should exist")
+		.into_jni_safe()
 }
 
 #[jni_call]
@@ -42,7 +98,11 @@ pub extern "C" fn JVM_SetCurrentThread(_env: JniEnv, _this: JObject, _thread: JO
 
 #[jni_call]
 pub extern "C" fn JVM_GetNextThreadIdOffset(_env: JniEnv, _class: JClass) -> jlong {
-	todo!()
+	// https://github.com/openjdk/jdk/blob/a3b58ee5cd1ec0ea78649d4128d272458b05eb13/src/java.base/share/classes/java/lang/Thread.java#L624-L627
+	const INITIAL_THREAD_ID: usize = 3;
+	static NEXT_THREAD_ID: AtomicUsize = AtomicUsize::new(INITIAL_THREAD_ID);
+
+	NEXT_THREAD_ID.as_ptr() as jlong
 }
 
 #[jni_call]
@@ -135,5 +195,5 @@ pub extern "C" fn JVM_EnsureMaterializedForStackWalk_func(
 	_vthread: JObject,
 	_value: JObject,
 ) {
-	todo!()
+	// Nothing to do
 }
