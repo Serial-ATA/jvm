e66fbba2f9244b58668a07b7879b9007af979c7e
Serial <69764315+Serial-ATA@users.noreply.github.com>
2026-07-31 11:24:25 -0400

    build: fallback to `JAVA_HOME` for `BOOT_JDK`

--- build/cli.py
+++ build/cli.py
@@ -1,9 +1,41 @@
 import os
+import re
+from typing import Optional
+
 import sys
 import argparse
 from argparse import Namespace
 
 from entry import VmVariant
+
+
+def check_release_file(java_home: str) -> bool:
+    expected_version = os.environ.get("JAVA_VERSION")
+    assert expected_version, "`JAVA_VERSION` should be set by `justfile`"
+
+    release_path = os.path.join(java_home, "release")
+    if not os.path.exists(release_path):
+        return False
+
+    with open(release_path) as f:
+        java_version_re = re.compile(f"JAVA_VERSION=\\\"({expected_version}\\.\\d+\\.\\d+)\\\"")
+        if not java_version_re.search(f.read()):
+            return False
+        return True
+
+
+def check_java_home() -> Optional[str]:
+    java_home = os.environ.get("JAVA_HOME")
+    if not java_home:
+        return None
+
+    if check_release_file(java_home):
+        print(
+            f"[WARN] Using `JAVA_HOME` ({java_home}) as boot JDK",
+            file=sys.stderr,
+        )
+        return java_home
+    return None
 
 
 def args() -> Namespace:
@@ -13,7 +45,7 @@ def args() -> Namespace:
     parser.add_argument(
         "--boot-jdk",
         type=str,
-        help="path to the boot JDK",
+        help="path to the boot JDK. if unspecified, `JAVA_HOME` will be checked",
     )
     parser.add_argument(
         "--profile",
@@ -42,6 +74,11 @@ def args() -> Namespace:
     if not ret.boot_jdk:
         boot_jdk_home = os.environ.get("BOOT_JDK")
         if boot_jdk_home is None:
+            boot_jdk_home = check_java_home()
+        else:
+            check_release_file(boot_jdk_home)
+
+        if not boot_jdk_home:
             print(
                 f"Boot JDK not specified (set `BOOT_JDK` environment variable or --boot-jdk)",
                 file=sys.stderr,
